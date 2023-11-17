f9f015353252a7a99441e1df8f5b3a155a3e1a4c
Jalsan <77244095+Sarylov@users.noreply.github.com>
2023-11-17 14:50:24 +0300

    add layout constructor with form

---
+++ src/layouts/constructor-with-form.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { Modal } from '../components/modal';
+import RevertIcon from '../assets/icons/revert.svg?react';
+
+// eslint-disable-next-line react/prop-types
+export const ConstructorWithForm = ({ form, constructor }) => {
+  const [isReverte, setIsReverte] = useState(false);
+
+  return (
+    <div className="w-full h-full flex justify-between">
+      <div className="flex-1 self-center hidden md:block">{form}</div>
+      <div className="flex-1 flex flex-col items-center h-[100vh] overflow-y-auto md:border-dotted md:border-l-2 border-gray-700 py-4">
+        <div className="flex items-start gap-2 w-full md:justify-between justify-center ">
+          <button
+            className="btn sticky ml-2 top-[50%] md:block hidden "
+            onClick={() => setIsReverte((prev) => !prev)}
+          >
+            <RevertIcon className="w-6" />
+          </button>
+          <div style={{ rotate: isReverte ? '180deg' : '0deg' }}>
+            {constructor}
+          </div>
+          <span></span>
+        </div>
+        <div className="flex justify-center gap-2 mt-4 md:hidden">
+          <Modal title={'Оформление'} name="Decoration" content={<>react</>}>
+            <button className="btn btn-primary">оформить заказ</button>
+          </Modal>
+          <button className="btn" onClick={() => setIsReverte((prev) => !prev)}>
+            <RevertIcon className="w-6" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
