0ccea12f61d89c3be1366dbeb4ab3270be317bcf
Jalsan <77244095+Sarylov@users.noreply.github.com>
2023-12-11 20:04:07 +0300

    add styles for modal window

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,10 +1,21 @@
-// eslint-disable-next-line react/prop-types
-export const Modal = ({ children, title, content, name, closeButton }) => {
+/* eslint-disable react/prop-types */
+export const Modal = ({
+  children,
+  title,
+  content,
+  name,
+  closeButton,
+  className = '',
+}) => {
+  function showModal() {
+    document.getElementById(name).showModal();
+  }
+
   return (
     <>
-      <div onClick={() => window[name].showModal()}>{children}</div>
+      <div onClick={showModal}>{children}</div>
       <dialog id={name} className="modal">
-        <div className="modal-box">
+        <div className={`modal-box ${className}`}>
           <form method="dialog">
             <h3 className="font-bold text-lg">{title}</h3>
             {content}
