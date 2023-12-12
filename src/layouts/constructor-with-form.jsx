54333be7636482567dcbdf5b5d00a66f94359394
Jalsan <77244095+Sarylov@users.noreply.github.com>
2023-12-12 05:02:19 +0300

    add constructor provider

--- src/layouts/constructor-with-form.jsx
+++ src/layouts/constructor-with-form.jsx
@@ -1,46 +1,50 @@
+/* eslint-disable react/prop-types */
 import { useContext } from 'react';
 import { Modal } from '../components/modal';
 import RevertIcon from '../assets/icons/revert.svg?react';
-import { BallConstructorContext } from '../contexts/ball-constructor-context';
-import { useScreen } from '../hooks/use-screen';
+import {
+  ConstructorContext,
+  ConstructorProvider,
+} from '../modules/constructorBall';
 
-// eslint-disable-next-line react/prop-types
-export const ConstructorWithForm = ({ form, constructor }) => {
-  const { isRotateConstructor, rotateConstructor } = useContext(
-    BallConstructorContext
-  );
-
-  const { isMobileScreen } = useScreen();
-
+const ButtonRotate = ({ className = 'btn' }) => {
+  const { rotateConstructor } = useContext(ConstructorContext);
   return (
-    <div className="w-full h-full flex justify-between">
-      {!isMobileScreen && (
+    <button className={className} onClick={rotateConstructor}>
+      <RevertIcon className="w-6" />
+    </button>
+  );
+};
+
+const RotatableConstructor = ({ constructor }) => {
+  const { isRotateConstructor } = useContext(ConstructorContext);
+  return (
+    <div style={{ rotate: isRotateConstructor ? '180deg' : '0deg' }}>
+      {constructor}
+    </div>
+  );
+};
+
+export const ConstructorWithForm = ({ form, constructor }) => {
+  return (
+    <ConstructorProvider>
+      <div className="w-full h-full flex justify-between">
         <div className="flex-1 self-center hidden md:block">{form}</div>
-      )}
-      <div className="flex-1 flex flex-col items-center h-[100vh] overflow-y-auto md:border-dotted md:border-l-2 border-gray-700 py-4">
-        <div className="flex items-start gap-2 w-full md:justify-between justify-center ">
-          <button
-            className="btn sticky ml-2 top-[50%] md:block hidden "
-            onClick={rotateConstructor}
-          >
-            <RevertIcon className="w-6" />
-          </button>
-          <div style={{ rotate: isRotateConstructor ? '180deg' : '0deg' }}>
-            {constructor}
+        <div className="flex-1 flex flex-col items-center h-[100vh] overflow-y-auto md:border-dotted md:border-l-2 border-gray-700 py-4">
+          <div className="flex items-start gap-2 w-full md:justify-between justify-center ">
+            <ButtonRotate className="btn sticky ml-2 top-[50%] md:block hidden" />
+            <RotatableConstructor constructor={constructor} />
+            <span></span>
           </div>
-          <span></span>
-        </div>
-        {isMobileScreen && (
+
           <div className="flex justify-center gap-2 mt-4 md:hidden">
             <Modal title={'Оформление'} name="Decoration" content={<>{form}</>}>
               <button className="btn btn-primary">Оформить заказ</button>
             </Modal>
-            <button className="btn" onClick={rotateConstructor}>
-              <RevertIcon className="w-6" />
-            </button>
+            <ButtonRotate />
           </div>
-        )}
+        </div>
       </div>
-    </div>
+    </ConstructorProvider>
   );
 };
