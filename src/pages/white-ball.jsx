5a28c60d121f5d4c82f735a4d205c4a27810404c
Jalsan <77244095+Sarylov@users.noreply.github.com>
2023-12-17 14:55:17 +0300

    add delivery on pages

--- src/pages/white-ball.jsx
+++ src/pages/white-ball.jsx
@@ -2,16 +2,19 @@ import { ContactForm } from '../components/contact-form';
 import { BallConstructorContext } from './../contexts/ball-constructor-context';
 import { useBall } from '../hooks/use-ball';
 import { ConstructorWithForm } from './../layouts/constructor-with-form';
+import { DeliveryProvider } from '../modules/delivery';
 import { ConstructorWhiteball } from '../modules/white-ball';
 
 export const Whiteball = () => {
   const contextValue = useBall();
   return (
     <BallConstructorContext.Provider value={contextValue}>
-      <ConstructorWithForm
-        form={<ContactForm />}
-        constructor={<ConstructorWhiteball />}
-      />
+      <DeliveryProvider>
+        <ConstructorWithForm
+          form={<ContactForm />}
+          constructor={<ConstructorWhiteball />}
+        />
+      </DeliveryProvider>
     </BallConstructorContext.Provider>
   );
 };
